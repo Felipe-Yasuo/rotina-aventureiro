e6df925b9a219f8a30a9795d186cb6622605ddd4
Felipe-Yasuo <felipe.yasuo@hotmail.com>
2025-11-10 14:50:40 -0300

    chore: inicializa projeto backend

---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+
+router.get("/ping", (req: Request, res: Response) => {
+    return res.json({ message: "pong" });
+});
+
+export { router };
+

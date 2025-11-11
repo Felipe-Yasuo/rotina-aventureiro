750f4253d30c644b6145c8724973b4a6651f1c06
Felipe-Yasuo <felipe.yasuo@hotmail.com>
2025-11-11 11:41:22 -0300

    feat: integra Prisma ORM com PostgreSQL e adiciona schema inicial do banco

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router, Request, Response } from "express";
+import { prisma } from "./prisma";
 
 const router = Router();
 
 
-router.get("/ping", (req: Request, res: Response) => {
-    return res.json({ message: "pong" });
+router.get("/users", async (req, res) => {
+    const users = await prisma.user.findMany();
+    return res.json(users);
 });
 
 export { router };
